85d8c9cbe0bd416f90bb280bb7a5ea7ff4af64fb
Raymond Yegon <raymondyegon17@gmail.com>
2019-04-01 09:19:04 +0300

    api updated

--- src/app/search-navbar/search-navbar.component.ts
+++ src/app/search-navbar/search-navbar.component.ts
@@ -1,13 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { GitsearchService } from './../github-service/github.service';
 
 @Component({
   selector: 'app-search-navbar',
   templateUrl: './search-navbar.component.html',
   styleUrls: ['./search-navbar.component.css']
 })
-export class SearchNavbarComponent implements OnInit {
+export class GitsearchComponent implements OnInit {
+    profile: any;
+    repos: any;
+    username: string;
 
-  constructor() { }
+  constructor(private gitsearchService: GitsearchService) {
+    
+   }
+
+   findProfile(){
+     this.gitsearchService.updateProfile(this.username);
+     this.gitsearchService.getProfileDesc().subscribe(profile =>{
+      console.log(profile);
+      this.profile = profile;
+    });
+    this.gitsearchService.getProfileRepos().subscribe(repos =>{
+      console.log(repos);
+      this.repos = repos;
+    })
+   }
 
   ngOnInit() {
   }
